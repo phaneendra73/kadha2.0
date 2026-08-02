7c289f7dd1e4bb802e90b703cbc2ff3a97a12307
phaneendra73 <phaneendra3377@gmail.com>
2026-08-02 12:29:28 +0530

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-DYQU6XRi.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-CvtoU94D.js";import{_ as a}from"./mermaid.core-BYb3SQpm.js";import"./chunk-5VM5RSS4-Bem9t-iN.js";import"./chunk-XXDRQBXY-DWhek-7h.js";import"./chunk-VR4S4FIN-CVhrFiZH.js";import"./chunk-32BRIVSS-c92FbFc-.js";import"./index-1YYBlmCD.js";import"./motion-DaPkh3EO.js";import"./vendor-CkIl0AP1.js";import"./icons-C3s5w5_H.js";import"./channel-B0i-6sov.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
